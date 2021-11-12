45a79199260ce2c1e85f2cb26a0c59be965393aa
Sharan Konda <sharansai.konda@sureify.com>
2021-11-12 09:49:20 +0530

    2048 game bug fix

--- 2048.py
+++ 2048.py
@@ -17,15 +17,38 @@ def fileWrite(fileName,mode,value):
     fp.write(value)
     fp.close()
 
-def initializeBoard():
+def setCustomBoard():
+    global board,rows,cols
+    rows = 2
+    cols = 2
+    board = [['-' for i in range(cols)] for j in range(rows)]
+    arr = [(0,0,2),(0,1,2),(1,0,4),(1,1,4)]
+    for r in arr:
+        if r[0] == 'X':
+            for x in range(rows):
+                board[x][r[1]] = r[2]
+        elif r[1] == 'X':
+            for y in range(cols):
+                board[r[0]][y] = r[2]
+        elif r[0] == 'X' and r[1] == 'X':
+            for x in range(rows):
+                for y in range(cols):
+                    board[x][y] = r[2]
+        else:
+            board[r[0]][r[1]] = r[2]
+
+def initializeBoard(isCustomBoard = False):
     
     fileWrite('result.txt','w','Board Moves\n-----------\n')   
     try:
-        readInput()
-        addRandomTile(2)
+        if isCustomBoard == False:
+            readInput()
+            addRandomTile(2)
+        else:
+            setCustomBoard()
         printBoard(None)
 
-        while isEmptyTileExists():
+        while isEmptyTileExists() or canMergeAny():
             if checkGameWon():
                 print('\nCongratulations! You won')
                 return
@@ -34,6 +57,7 @@ def initializeBoard():
             moveAllTiles(direction)
             addRandomTile(1)
             printBoard(direction)
+        print('\nGame Over')
     except Exception as e:
         print(e)
 
@@ -54,47 +78,46 @@ def getDirection():
         else:
             print('\nInvalid direction chosen. Please try again')
         
+def areTilesMovable(row_step1,col_step1,row_step2,col_step2,rows,cols,isInvert=False):
+    for colIdx in range(cols):
+        for rowIdx in range(rows-1):
+            idx1 = rowIdx+row_step1
+            idx2 = colIdx+col_step1
+            idx3 = rowIdx+row_step2
+            idx4 = colIdx+col_step2
+
+            if isInvert:
+                idx1, idx2 = idx2, idx1
+                idx3, idx4 = idx4, idx3
+
+            if board[idx1][idx2] == '-' and board[idx3][idx4] == '-':
+                    continue
+            elif board[idx1][idx2] == '-' and board[idx3][idx4] != '-':
+                return True
+            elif board[idx1][idx2] == board[idx3][idx4] and board[idx1][idx2] != '-':
+                return True
+    return False
+
 def canMoveTiles(direction):
     global board, rows, cols
     if direction == TOP:
-        for colIdx in range(cols):
-            for rowIdx in range(rows-1):
-                if board[rowIdx][colIdx] == '-' and board[rowIdx+1][colIdx] == '-':
-                    continue
-                elif board[rowIdx][colIdx] == '-' and board[rowIdx+1][colIdx] != '-':
-                    return True
-                elif board[rowIdx][colIdx] == board[rowIdx+1][colIdx] and board[rowIdx][colIdx] != '-':
-                    return True
-    elif direction == BOTTOM:
-        for colIdx in range(cols):
-            for rowIdx in range(rows-1):
-                if board[rowIdx][colIdx] == '-' and board[rowIdx+1][colIdx] == '-':
-                    continue
-                elif board[rowIdx][colIdx] != '-' and board[rowIdx+1][colIdx] == '-':
-                    return True
-                elif board[rowIdx][colIdx] == board[rowIdx+1][colIdx] and board[rowIdx][colIdx] != '-':
-                    return True
+        return areTilesMovable(0,0,1,0,rows,cols)
     
-    elif direction == LEFT:
-         for rowIdx in range(rows):
-            for colIdx in range(cols-1):
-                if board[rowIdx][colIdx] == '-' and board[rowIdx][colIdx+1] == '-':
-                    continue
-                elif board[rowIdx][colIdx] == '-' and board[rowIdx][colIdx+1] != '-':
-                    return True
-                elif board[rowIdx][colIdx] == board[rowIdx][colIdx+1] and board[rowIdx][colIdx] != '-':
-                    return True
-    else:
-        for rowIdx in range(rows):
-            for colIdx in range(cols-1):
-                if board[rowIdx][colIdx] == '-' and board[rowIdx][colIdx+1] == '-':
-                    continue
-                elif board[rowIdx][colIdx] != '-' and board[rowIdx][colIdx+1] == '-':
-                    return True
-                elif board[rowIdx][colIdx] == board[rowIdx][colIdx+1] and board[rowIdx][colIdx] != '-':
-                    return True
-    return False
+    if direction == BOTTOM:
+        return areTilesMovable(1,0,0,0,rows,cols)
+        
+    if direction == LEFT:
+        return  areTilesMovable(0,0,1,0,cols,rows,True)
+    
+    return areTilesMovable(1,0,0,0,cols,rows,True)
 
+def canMergeAny():
+    global board, rows, cols
+    return areTilesMovable(0,0,1,0,rows,cols) or \
+         areTilesMovable(1,0,0,0,rows,cols) or \
+             areTilesMovable(0,0,1,0,cols,rows,True) or \
+                 areTilesMovable(1,0,0,0,cols,rows,True)
+    
 def moveAllTiles(direction):
     if direction == TOP:
         moveTilesTop()
@@ -109,6 +132,7 @@ def moveTilesTop():
     global rows, cols, board
     colIdx = cols - 1
     while colIdx >= 0:
+        shiftTilesTop(colIdx)
         rowIdx = 0
         while rowIdx < rows - 1:
             if board[rowIdx][colIdx] == board[rowIdx+1][colIdx]:
@@ -127,6 +151,7 @@ def moveTilesBottom():
     global rows, cols, board
     colIdx = cols - 1
     while colIdx >= 0:
+        shiftTilesBottom(colIdx)
         rowIdx = rows - 1
         while rowIdx > 0:
             if board[rowIdx][colIdx] == board[rowIdx-1][colIdx]:
@@ -145,6 +170,7 @@ def moveTilesLeft():
     global rows, cols, board
     rowIdx = rows - 1
     while rowIdx >= 0:
+        shiftTilesLeft(rowIdx)
         colIdx = 0
         while colIdx < cols - 1:
             if board[rowIdx][colIdx] == board[rowIdx][colIdx+1]:
@@ -163,6 +189,7 @@ def moveTilesRight():
     global rows, cols, board
     rowIdx = rows - 1
     while rowIdx >= 0:
+        shiftTilesRight(rowIdx)
         colIdx = cols - 1
         while colIdx > 0:
             if board[rowIdx][colIdx] == board[rowIdx][colIdx-1]:
@@ -179,68 +206,84 @@ def moveTilesRight():
 
 def shiftTilesTop(colIdx):
     global board, rows
-    while True:
-        isMoved = False
-        for idx in range(rows-1):
-            if board[idx][colIdx] == '-' and  board[idx][colIdx] == board[idx+1][colIdx]:
-                continue
-            
-            if board[idx][colIdx] == '-':
-                isMoved = True
-                board[idx][colIdx] = board[idx+1][colIdx]
-                board[idx+1][colIdx] = '-'
+    i = 0
+    while i < rows - 1:
+        if board[i][colIdx] != '-':
+            i+=1
+            continue
 
-        if isMoved == False:
+        j = i+1
+        while j<rows and board[j][colIdx] == '-':
+            j+=1
+        
+        if j != rows:
+            board[i][colIdx] = board[j][colIdx]
+            board[j][colIdx] = '-'
+            i = j
+            j += 1
+        else:
             break
+    
 
 def shiftTilesBottom(colIdx):
     global board, rows
-    while True:
-        isMoved = False
-        for idx in range(rows-1,0,-1):
-            if board[idx][colIdx] == '-' and board[idx][colIdx] == board[idx-1][colIdx]:
-                continue
+    i = rows - 1
+    while i > 0:
+        if board[i][colIdx] != '-':
+            i-=1
+            continue
 
-            if board[idx][colIdx] == '-':
-                isMoved = True
-                board[idx][colIdx] = board[idx-1][colIdx]
-                board[idx-1][colIdx] = '-'
-            
-        if isMoved == False:
+        j = i-1
+        while j>=0 and board[j][colIdx] == '-':
+            j-=1
+        
+        if j >= 0:
+            board[i][colIdx] = board[j][colIdx]
+            board[j][colIdx] = '-'
+            i = j
+            j -= 1
+        else:
             break
 
 def shiftTilesLeft(rowIdx):
     global board, cols
-    while True:
-        isMoved = False
-        for idx in range(cols-1):
-            if board[rowIdx][idx] == '-' and  board[rowIdx][idx] == board[rowIdx][idx+1]:
-                continue
-            
-            
-            if board[rowIdx][idx] == '-':
-                isMoved = True
-                board[rowIdx][idx] = board[rowIdx][idx+1]
-                board[rowIdx][idx+1] = '-'
-             
-        if isMoved == False:
+    i = 0
+    while i < cols - 1:
+        if board[rowIdx][i] != '-':
+            i+=1
+            continue
+
+        j = i+1
+        while j<cols and board[rowIdx][j] == '-':
+            j+=1
+        
+        if j != cols:
+            board[rowIdx][i] = board[rowIdx][j]
+            board[rowIdx][j] = '-'
+            i = j
+            j += 1
+        else:
             break
 
 def shiftTilesRight(rowIdx):
     global board, cols
-    while True:
-        isMoved = False
-        for idx in range(cols-1, 0, -1):
-            if board[rowIdx][idx] == '-' and  board[rowIdx][idx] == board[rowIdx][idx-1]:
-                continue
-            
-            if board[rowIdx][idx] == '-':
-                isMoved = True
-                board[rowIdx][idx] = board[rowIdx][idx-1]
-                board[rowIdx][idx-1] = '-'
+    i = cols - 1
+    while i > 0:
+        if board[rowIdx][i] != '-':
+            i-=1
+            continue
+
+        j = i-1
+        while j>=0 and board[rowIdx][j] == '-':
+            j-=1
         
-        if isMoved == False:
-                break
+        if j >=0:
+            board[rowIdx][i] = board[rowIdx][j]
+            board[rowIdx][j] = '-'
+            i = j
+            j -= 1
+        else:
+            break
 
 def isEmptyTileExists():
     global board
